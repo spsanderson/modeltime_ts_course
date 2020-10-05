305472a7a71199a49a74ed4baa5ed86ae5589e46
Steven Sanderson <spsanderson@gmail.com>
2020-10-05 14:26:21 -0400

    starting script

---
+++ 01_jumpstart/00_data_overview.R
@@ -0,0 +1,28 @@
+# BUSINESS SCIENCE UNIVERSITY
+# DS4B 203-R: ADVANCED TIME SERIES FORECASTING FOR BUSINESS
+# MODULE: INSPECT COURSE DATASETS
+
+
+# LIBRARIES ----
+library(tidyverse)
+
+
+# DATA -----
+
+# * Establish Relationships ----
+#   - Website traffic (Page Views, Sessions, Organic Traffic)
+#   - Top 20 Pages
+
+
+
+# * Build Relationships ----
+#   - Collect emails
+#   - Host Events
+
+
+
+# * Generate Course Revenue ----
+#   - Revenue data (aggregated at weekly interval)
+#   - Product Events
+
+

a8bc9649301609c79dcf7767d56f4b5cbe2070f2
Steven Sanderson <spsanderson@gmail.com>
2020-10-05 14:36:20 -0400

    view data sets

--- 01_jumpstart/00_data_overview.R
+++ 01_jumpstart/00_data_overview.R
@@ -13,13 +13,26 @@ library(tidyverse)
 #   - Website traffic (Page Views, Sessions, Organic Traffic)
 #   - Top 20 Pages
 
-
+# Google Analytics - Summary Hourly
+read_rds("00_data/google_analytics_summary_hourly.rds")
+# Google Analytics - Top 20 Pages, daily
+read_rds("00_data/google_analytics_by_page_daily.rds")
 
 # * Build Relationships ----
 #   - Collect emails
 #   - Host Events
 
+# Mailchimp Data
+read_rds("00_data/mailchimp_users.rds")
 
+# Learning Labs
+read_rds("00_data/learning_labs.rds")
+
+# Transactions Weekly
+read_rds("00_data/transactions_weekly.rds")
+
+# Product events
+read_rds("00_data/product_events.rds")
 
 # * Generate Course Revenue ----
 #   - Revenue data (aggregated at weekly interval)

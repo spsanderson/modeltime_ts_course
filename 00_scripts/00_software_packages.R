767f2f09b89dc16854900207b0dc3f9c9c51aeb4
Steven Sanderson <spsanderson@gmail.com>
2020-10-05 14:26:07 -0400

    course lib dependencies

---
+++ 00_scripts/00_software_packages.R
@@ -0,0 +1,36 @@
+# BUSINESS SCIENCE UNIVERSITY
+# DS4B 203-R: ADVANCED  TIME SERIES FORECASTING FOR BUSINESS
+# MODULE: Software Installation
+
+# LIBRARIES ----
+
+# * Forecasting Libraries ----
+library(forecast)     # Auto ARIMA, ETS
+library(prophet)      # FB Prophet
+
+# * Machine Learning Libraries ----
+library(glmnet)       # Elastic Net
+library(earth)        # Mars Regression Splines
+library(kernlab)      # Support Vector Machine
+library(kknn)         # K-Nearest Neighbors
+library(randomForest) # Random Forest
+library(ranger)       # Random Forest
+library(xgboost)      # Boosted Trees
+library(Cubist)       # Cubist Rule-Based Algorithm
+
+# * Deep Learning Libraries ----
+library(reticulate)   # Python interface
+
+# * Time Series ML ----
+library(tidymodels)   # Meta - workflows, parsnip, tune, dials, recipes, rsample, yardstick
+library(rules)        # Rule-based models (cubist)
+library(modeltime)    # tidymodels time series extension
+
+# * Core Libraries ----
+library(tidyverse)    # Meta - dplyr, ggplot2, purrr, tidyr, stringr, forcats
+library(lubridate)    # date and time
+library(timetk)       # Time series data wrangling, visualization and preprocessing
+
+# Extras
+library(DataExplorer)
+library(fs)

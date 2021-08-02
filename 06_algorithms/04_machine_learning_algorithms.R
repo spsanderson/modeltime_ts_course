5c3168537bea0e18a97b126855c5089411e6a641
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2021-08-02 15:16:48 -0400

    Update 04_machine_learning_algorithms.R

--- 06_algorithms/04_machine_learning_algorithms.R
+++ 06_algorithms/04_machine_learning_algorithms.R
@@ -184,31 +184,76 @@ calibrate_and_plot(
 # Strengths: Well-rounded algorithm
 # Weaknesses: Needs tuned or can overfit
 
+model_spec_svm_poly <- svm_poly(
+  mode = "regression",
+  cost = 10
+) %>%
+  set_engine("kernlab")
 
 
 # Spline
+set.seed(123)
 
+wflw_fit_svm_poly_spline <- workflow() %>%
+  add_model(model_spec_svm_poly) %>%
+  add_recipe(recipe_spec_1_spline) %>%
+  fit(training(splits))
+
+calibrate_and_plot(
+  wflw_fit_svm_poly_spline,
+  type = "testing"
+)
 
 # Lag
+set.seed(123)
+wflw_fit_svm_poly_lag <- workflow() %>%
+  add_model(model_spec_svm_poly) %>%
+  add_recipe(recipe_spec_2_lag) %>%
+  fit(training(splits))
 
+calibrate_and_plot(
+  wflw_fit_svm_poly_lag,
+  type = "testing"
+)
 
 # Calibrate & Plot
 
-
+calibrate_and_plot(
+  wflw_fit_svm_poly_spline,
+  wflw_fit_svm_poly_lag
+)
 
 # 4.0 SVM RADIAL BASIS ----
 # Strengths: Well-rounded algorithm
 # Weaknesses: Needs tuned or can overfit
 
+model_spec_svm_rbf <- svm_rbf(
+  mode = "regression",
+  cost = 1,
+  rbf_sigma = 0.01,
+  margin = 0.1
+) %>%
+  set_engine("kernlab")
 
 # Spline
-
+wflw_fit_svm_rbf_spline <- workflow() %>%
+  add_model(model_spec_svm_rbf) %>%
+  add_recipe(recipe_spec_1_spline) %>%
+  fit(training(splits))
 
 # Lag
+wflw_fit_svm_rbf_lag <- workflow() %>%
+  add_model(model_spec_svm_rbf) %>%
+  add_recipe(recipe_spec_2_lag) %>%
+  fit(training(splits))
 
 
 # Calibrate & Plot
 
+calibrate_and_plot(
+  wflw_fit_svm_rbf_spline,
+  wflw_fit_svm_rbf_lag
+)
 
 
 # 5.0 K-NEAREST NEIGHBORS ----
@@ -219,16 +264,67 @@ calibrate_and_plot(
 
 # Show issue when trend extends beyond maximum value ----
 
+sample_data_tbl <- tibble(
+  date = tk_make_timeseries("2011", by = "quarter", length_out = 10),
+  value = 1:10
+)
+
+model_spec_knn_simple <- nearest_neighbor(mode = "regression") %>%
+  set_engine("kknn")
+
+model_spec_glmnet_simple <- linear_reg(penalty = 0.1, mixture = 0.5) %>%
+  set_engine("glmnet")
+
+simple_fit_knn <- model_spec_knn_simple %>%
+  fit(value ~ as.numeric(date), sample_data_tbl)
+
+simple_fit_glmnet <- model_spec_glmnet_simple %>%
+  fit(value ~ as.numeric(date) + quarter(date), sample_data_tbl)
+
+modeltime_table(
+  simple_fit_knn,
+  simple_fit_glmnet
+) %>%
+  modeltime_forecast(
+    new_data = bind_rows(
+      sample_data_tbl, 
+      future_frame(sample_data_tbl, .length_out = "2 years")
+    ),
+    actual_data = sample_data_tbl
+  ) %>%
+  plot_modeltime_forecast()
 
 # Implementation ----
 
-# Spline
+model_spec_knn <- nearest_neighbor(
+    mode = "regression",
+    neighbors = 50,
+    dist_power = 10,
+    weight_func = "optimal"
+  ) %>%
+  set_engine("kknn")
 
+# Spline
+set.seed(123)
+wflw_fit_knn_spline <- workflow() %>%
+  add_model(model_spec_knn) %>%
+  add_recipe(recipe_spec_1_spline) %>%
+  fit(training(splits))
 
 # Lag
-
+set.seed(123)
+wflw_fit_knn_lag <- workflow() %>%
+  add_model(model_spec_knn) %>%
+  add_recipe(recipe_spec_2_lag) %>%
+  fit(training(splits))
 
 # Calibrate & Plot
+
+calibrate_and_plot(
+  wflw_fit_knn_spline,
+  wflw_fit_knn_lag,
+  type = "testing"
+)
 
 # 6.0 RANDOM FOREST ----
 # - Strengths: Can model seasonality very well

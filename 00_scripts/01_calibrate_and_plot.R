9e0690cdd3dc44cad42b8804c24d2ca75c5e2f3b
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2021-08-09 11:11:39 -0400

    Create 01_calibrate_and_plot.R

---
+++ 00_scripts/01_calibrate_and_plot.R
@@ -0,0 +1,24 @@
+calibrate_and_plot <-
+function(..., type = "testing"){
+  
+  if(type == "testing"){
+    new_data = testing(splits)
+  } else {
+    new_data = training(splits) %>%
+      drop_na()
+  }
+  
+  calibration_tbl <- modeltime_table(...) %>%
+    modeltime_calibrate(new_data)
+  
+  print(calibration_tbl %>% modeltime_accuracy())
+  
+  calibration_tbl %>%
+    modeltime_forecast(
+      new_data = new_data
+      , actual_data = data_prepared_tbl
+    ) %>%
+    plot_modeltime_forecast(
+      .conf_interval_show = FALSE
+    )
+}
